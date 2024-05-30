ad263f577288a93de5eba4620e529c0aec4fb2f5
yyh <1714564639@qq.com>
2024-05-30 11:38:22 +0800

    test

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    return "hello_world"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
